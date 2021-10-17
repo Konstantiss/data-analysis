6ce4d62210107503a68d6637e957dba33256352b
Konstantinos Michopoulos <konstantinos.michopoulos@gmail.com>
2021-10-17 14:31:43 +0000

    Change uniformNumbers initialisation size

--- Data_Anal_1st_Week_4.m
+++ Data_Anal_1st_Week_4.m
@@ -3,7 +3,7 @@ a = 1
 b = 2
 means = zeros(n, 2)
 for i = 1:n
-    uniformNumbers = unifrnd(a, b, 1, 10000)
+    uniformNumbers = unifrnd(a, b, 1, i)
     means(i, 1) = 1 / mean(uniformNumbers)
     means(i, 2) = mean(1 ./ uniformNumbers)
 end
