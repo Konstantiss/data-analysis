db4d7032ac54ce389e0d333f10107627b44c4d29
Konstantiss <konstantinos.michopoulos@gmail.com>
2021-10-24 18:31:48 +0300

    Increase number of attempts.

--- Data_Anal_Chapter2_4.m
+++ Data_Anal_Chapter2_4.m
@@ -1,11 +1,11 @@
-n = 100
+n = 300
 a = 1
 b = 2
-means = zeros(n, 2)
+means = zeros(n, 2);
 for i = 1:n
-    uniformNumbers = unifrnd(a, b, 1, i)
-    means(i, 1) = 1 / mean(uniformNumbers)
-    means(i, 2) = mean(1 ./ uniformNumbers)
+    uniformNumbers = unifrnd(a, b, 1, i);
+    means(i, 1) = 1 / mean(uniformNumbers);
+    means(i, 2) = mean(1 ./ uniformNumbers);
 end
 plot(means)
 
