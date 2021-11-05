01e5bdbd590976f42794be8184b1f99607673078
Konstantinos Michopoulos <konstantinos.michopoulos@gmail.com>
2021-11-05 16:20:51 +0000

    Correct chapter 3 excercise 3 solution.

--- Data_Anal_Chapter3_3.m
+++ Data_Anal_Chapter3_3.m
@@ -1,20 +1,43 @@
+%first question
 M = 1000;
 n = 5;
 mu = 15;
 
-exp = exprnd(mu, M, n);
+exp = exprnd(mu, n, M);
+
+expMean = mean(exp);
 
 [h,p,ci,stats] = ttest(exp);
-ci;
+ciMean(1,1) = mean(ci(1,:));
+ciMean(2,1) = mean(ci(2,:));
 counter = 0;
 
 for i=1:1:M
-    for j=1:1:n
-        if exp(i, j) < ci(2, j) && exp(i, j) > ci(1, j)
-            counter = counter + 1;
-        end
+    if expMean(1, i) < ciMean(2, 1) && expMean(1, i) > ciMean(1, 1)
+        counter = counter + 1;
     end
 end
 
-counter
-succesRate = counter / (M * n) 
+succesRatePercentage = (counter / M) * 100 
+%second question
+M = 1000;
+n = 100;
+mu = 15;
+
+exp = exprnd(mu, n, M);
+
+expMean = mean(exp);
+
+[h,p,ci,stats] = ttest(exp);
+ciMean(1,1) = mean(ci(1,:));
+ciMean(2,1) = mean(ci(2,:));
+counter = 0;
+
+for i=1:1:M
+    if expMean(1, i) < ciMean(2, 1) && expMean(1, i) > ciMean(1, 1)
+        counter = counter + 1;
+    end
+end
+
+succesRatePercentage = (counter / M) * 100
+%The succes rate is reduced for higher ns.
