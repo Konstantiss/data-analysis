b47d1cb2796eb63840ee080e90c7df7531fa7314
Konstantinos Michopoulos <konstantinos.michopoulos@gmail.com>
2021-11-07 15:26:36 +0000

    Run the same procedure for multiple ns.

--- Data_Anal_Chapter3_1.m
+++ Data_Anal_Chapter3_1.m
@@ -1,17 +1,24 @@
+%a
+M = 1000;
 n = 1000;
 lambda = 100;
+ns = 2.^[2:12]';
+for i=1:length(ns)
+    poiss = poissrnd(lambda, 1, ns(i));
 
-poiss = poissrnd(lambda, 1, n);
+    maxLikelihoodEstimate = mle(poiss, 'distribution', 'poisson');
 
-maxLikelihoodEstimate = mle(poiss, 'distribution', 'poisson');
+    %Prove that they're equal
+    maxLikelihoodEstimate == mean(poiss) 
+end
 
-%Prove that they're equal
-maxLikelihoodEstimate == mean(poiss)
 
-y = 1/n * sum(poiss);
-
-histogram(y);
-
-histfit(exp(:,1),20,'poisson')
-
-mean(y)
+%b
+for i=1:length(ns)
+    figure(i);
+    poiss = poissrnd(lambda,M,ns(i));
+    y = 1/n * sum(poiss);
+    histogram(y)
+    histfit(poiss(:,1),20,'poisson')
+    mean(y)
+end
