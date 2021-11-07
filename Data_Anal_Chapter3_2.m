12a1d0f00c8b624a4ec1b06272f6ff6e3c002126
Konstantinos Michopoulos <konstantinos.michopoulos@gmail.com>
2021-11-07 15:27:49 +0000

    Run the same procedure for multiple ns.

--- Data_Anal_Chapter3_2.m
+++ Data_Anal_Chapter3_2.m
@@ -1,18 +1,24 @@
+%a
+M = 1000;
 n = 1000;
 lambda = 100;
+ns = 2.^[2:12]';
+for i=1:length(ns)
+    exp = exprnd(1/lambda, 1, ns(i)); %E[X] = 1 / lambda
 
-exp = exprnd(1/lambda, 1, n); %E[X] = 1 / lambda
+    maxLikelihoodEstimate = mle(exp, 'distribution', 'exponential');
 
-maxLikelihoodEstimate = mle(exp, 'distribution', 'exponential');
+    %Prove that they're equal
+    maxLikelihoodEstimate == mean(exp) 
+end
 
-%Prove that they're equal
-maxLikelihoodEstimate == mean(exp)
 
-
-y = 1/n * sum(exp);
-
-histogram(y);
-
-histfit(exp(:,1),20,'exponential')
-
-mean(y)
+%b
+for i=1:length(ns)
+    figure(i);
+    exp = exprnd(1/lambda,M,ns(i));
+    y = 1/n * sum(exp);
+    histogram(y)
+    histfit(exp(:,1),20,'exponential')
+    mean(y)
+end
