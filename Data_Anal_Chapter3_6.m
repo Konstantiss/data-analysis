104590f7aa3fe868fe8ac4d44acafb6e43bb1b4d
Konstantinos Michopoulos <konstantinos.michopoulos@gmail.com>
2021-11-13 17:30:22 +0000

    Add excercise 6 chapter 3 solution.

---
+++ Data_Anal_Chapter3_6.m
@@ -0,0 +1,24 @@
+X = rand(10,1)
+
+%First question:
+bootstatX = bootstrp(1000,@mean,X);
+histogram(bootstat,50)
+hold on
+grid on
+histogram(X,50)
+hold off
+
+%Second question:
+std(bootstatX)
+std(X)
+
+%Third question:
+Y = exp(X);
+bootstatY = bootstrp(1000,@mean,Y);
+histogram(bootstat,50)
+hold on
+grid on
+histogram(Y,50)
+hold off
+std(bootstatY)
+std(Y)
