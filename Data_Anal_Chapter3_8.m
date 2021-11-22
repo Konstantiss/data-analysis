65ebe3222e3410ce7ca6a9209c2052d57247cddd
Konstantinos Michopoulos <konstantinos.michopoulos@gmail.com>
2021-11-22 11:06:06 +0000

    Change array size and replace rand with normrnd

--- Data_Anal_Chapter3_8.m
+++ Data_Anal_Chapter3_8.m
@@ -1,28 +1,38 @@
 clc
 clear
-
-X = rand(10,100);
-stdCisX = zeros(10,2);
+n = 10;
+m = 100;
+X = normrnd(0,1,n,m);
 
 %First question:
 
-for i=1:1:10
-    [h,p,ci,stats] = vartest(X(i,:), 10);
-    stdCisX(i,:) = sqrt(ci);
+for i=1:m
+    [h,p,ci,stats] = vartest(X(:,i), n);
+    stdCisX(:,i) = ci;
 end
-ciBootX = bootci(100,@std,X);
+
+stdCisX = sqrt(stdCisX);
+ciBootX = bootci(m,@std,X);
 
 figure(1)
-histogram(stdCisX)
-hold on 
-histogram(ciBootX)
+histogram(stdCisX,50)
+hold on
+grid on
+histogram(ciBootX,50)
 hold off
 
 %Second question:
 Y = X.^2;
-for i=1:1:10
-    [h,p,ci,stats] = vartest(Y(i,:), 10);
-    stdCisY(i,:) = sqrt(ci);
+for i=1:m
+    [h,p,ci,stats] = vartest(Y(:,i), n);
+    stdCisY(:,i) = sqrt(ci);
 end
 
-ciBootY = bootci(100,@std,Y);
+ciBootY = bootci(m,@std,Y);
+
+figure(2)
+histogram(stdCisY,50)
+hold on
+grid on
+histogram(ciBootY,50)
+hold off
