1779dfe95f473ae8156945c91791b57c6436581c
Konstantinos Michopoulos <konstantinos.michopoulos@gmail.com>
2021-11-22 09:06:53 +0000

    Add histograms.

--- Data_Anal_Chapter3_7.m
+++ Data_Anal_Chapter3_7.m
@@ -8,8 +8,23 @@ X = rand(10,100);
 
 ciBootX = bootci(100,@mean,X);
 
+figure(1)
+histogram(ciX,50)
+hold on
+grid on
+histogram(ciBootX,50)
+hold off
+
 %Second question:
 Y = X.^2;
+
 [hY,pY,ciY,statsY] = ttest(Y);
 
 ciBootY = bootci(100,@mean,Y);
+
+figure(2)
+histogram(ciY,50)
+hold on
+grid on
+histogram(ciBootY,50)
+hold off
