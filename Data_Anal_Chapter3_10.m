60ed4c7c96fc2d0e201c6be36554760cbc10dea1
Konstantinos Michopoulos <konstantinos.michopoulos@gmail.com>
2021-11-24 11:01:48 +0000

    Add chapter 3 exercise 10 solution.

---
+++ Data_Anal_Chapter3_10.m
@@ -0,0 +1,34 @@
+clc
+clear
+
+n = 10;
+M = 100;
+m = 12;
+
+X = normrnd(0,1,M,n);
+
+Y = normrnd(0,1,M,m);
+
+allValues = zeros(100,22);
+
+allValues(1:100,1:10) = X;
+allValues(1:100,11:22) = Y;
+
+iterations = 1000;
+pValues = zeros(1,100);
+for i=1:M
+    for j=1:iterations
+       meanDifference1 = mean(X(i,:)) - mean(Y(i,:));
+       shuffledSample = allValues(i,:);
+       shuffledSample=shuffledSample(randperm(length(shuffledSample)));
+       newX = shuffledSample(1:n);
+       newY = shuffledSample((n+1):m);
+       meanDifference2 = mean(newX) - mean(newY);
+       if abs(meanDifference2) >= meanDifference1
+           meanDifferenceResults(j) = 1;
+       else
+           meanDifferenceResults(j) = 0;
+       end
+    end
+    pValues(1,i) = sum(meanDifferenceResults(:) == 1) / length(meanDifferenceResults);
+end
