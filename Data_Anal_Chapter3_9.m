120f26de6c5390386d8907872be6f7cd84833c03
Konstantinos Michopoulos <konstantinos.michopoulos@gmail.com>
2021-11-25 11:01:29 +0000

    Correct chapter 3 excercise 9 solution and add figures.

--- Data_Anal_Chapter3_9.m
+++ Data_Anal_Chapter3_9.m
@@ -4,48 +4,120 @@ clear
 n = 10;
 M = 100;
 m = 12;
+mu = 0;
+B = 1000;
+a = 0.05;
+sigma = 1;
+nbins= 20;
 
-X = normrnd(0,1,M,n);
+X = normrnd(mu,sigma,M,n);
 
-Y = normrnd(0,1,M,m);
+Y = normrnd(mu,sigma,M,m);
 
-for i=1:M
-   means(i) = mean(X(i,:)) - mean(Y(i,:)); 
-end
+cis = NaN(M,2);
+ciBootstrap = NaN(M,2); 
+
+lowerLimit = (B+1)*(a/2);
+upperLimit = (B+1 - lowerLimit); 
+limits = floor([lowerLimit,upperLimit]);
+
+
 
 %First question:
 
 %i:
-[~,~,ciDiff,~] = ttest(means);
+for i=1:M
+    %i:
+    [h,p,ci,stats] = ttest2(X(i,:),Y(i,:));
+    cis(i,1) = ci(1);
+    cis(i,2) = ci(2);
+    %ii:
+    bootstrapMeanX = bootstrp(B,@mean,X(i,:));
+    bootstrapMeanY = bootstrp(B,@mean,Y(i,:)); 
+    difference = bootstrapMeanX - bootstrapMeanY;
+    difference = sort(difference);
+    ciBootstrap(i,:) = difference(limits);
+end
 
-%ii:
-ciDiffBoot = bootci(100,@mean,means);
+figure(1)
+clf;
+histfit(cis(:,1),nbins);
+hold on
+histfit(cis(:,2),nbins);
+legend('lower limit','', 'uper limit','')
 
-percentage(1) = abs((ciDiffBoot(1,1) - ciDiff(1))) * 100;
-percentage(2) = abs((ciDiffBoot(2,1) - ciDiff(2))) * 100;
-
+figure(2)
+clf;
+histfit(ciBootstrap(:,1),nbins);
+hold on
+histfit(ciBootstrap(:,2),nbins);
+legend('lower limit','', 'uper limit','')
 
 %Second question:
 
 X2 = X.^2;
 Y2 = Y.^2;
+cis2 = NaN(M,2);
+ciBootstrap2 = NaN(M,2); 
+
 
 for i=1:M
-   means2(i) = mean(X2(i,:)) - mean(Y2(i,:)); 
+    %i:
+    [h,p,ci,stats] = ttest2(X2(i,:),Y2(i,:));
+    cis2(i,1) = ci(1);
+    cis2(i,2) = ci(2);
+    %ii:
+    bootstrapMeanX = bootstrp(B,@mean,X2(i,:));
+    bootstrapMeanY = bootstrp(B,@mean,Y2(i,:)); 
+    difference = bootstrapMeanX - bootstrapMeanY;
+    difference = sort(difference);
+    ciBootstrap2(i,:) = difference(limits);
 end
 
-[~,~,ciDiff2,~] = ttest(means2);
+figure(3)
+clf;
+histfit(cis2(:,1),nbins);
+hold on
+histfit(cis2(:,2),nbins);
+legend('lower limit','', 'uper limit','')
 
-ciDiff2Boot = bootci(100,@mean,means2);
+figure(4)
+clf;
+histfit(ciBootstrap2(:,1),nbins);
+hold on
+histfit(ciBootstrap2(:,2),nbins);
+legend('lower limit','', 'uper limit','')
 
 %Third question:
 
 Y = normrnd(0.2,1,M,m);
+cis3 = NaN(M,2);
+ciBootstrap3 = NaN(M,2); 
+
 
 for i=1:M
-   means3(i) = mean(X(i,:)) - mean(Y(i,:)); 
+    %i:
+    [h,p,ci,stats] = ttest2(X(i,:),Y(i,:));
+    cis3(i,1) = ci(1);
+    cis3(i,2) = ci(2);
+    %ii:
+    bootstrapMeanX = bootstrp(B,@mean,X(i,:));
+    bootstrapMeanY = bootstrp(B,@mean,Y(i,:)); 
+    difference = bootstrapMeanX - bootstrapMeanY;
+    difference = sort(difference);
+    ciBootstrap3(i,:) = difference(limits);
 end
 
-[~,~,ciDiff3,~] = ttest(means3);
+figure(5)
+clf;
+histfit(cis3(:,1),nbins);
+hold on
+histfit(cis3(:,2),nbins);
+legend('lower limit','', 'uper limit','')
 
-ciDiff3Boot = bootci(100,@mean,means3);
+figure(6)
+clf;
+histfit(ciBootstrap3(:,1),nbins);
+hold on
+histfit(ciBootstrap3(:,2),nbins);
+legend('lower limit','', 'uper limit','')
