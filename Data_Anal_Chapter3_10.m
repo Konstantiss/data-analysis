6d755f8747386aee5ec2854a6e90be66861c7f5e
Konstantinos Michopoulos <konstantinos.michopoulos@gmail.com>
2021-11-26 11:14:27 +0000

    Change ch03 ex10 solution.

--- Data_Anal_Chapter3_10.m
+++ Data_Anal_Chapter3_10.m
@@ -1,37 +1,56 @@
 clc
 clear
 
-n = 10;
+mu = 0;
+sigma = 1;
 M = 100;
-m = 12;
+n = 10;
+B = 1000;
+a = [0.05, 0.025, 0.1];
+%for question beta change xsquared to 1
+xSquared = 0;
 
-X = normrnd(0,1,M,n);
+if xSquared == 1
+    X = X.^2;
+end
 
-Y = normrnd(0,1,M,m);
+X = normrnd(mu,sigma,M,n);
 
-allValues = zeros(100,22);
+pValues0 = NaN(1,M);
+pValues05 = NaN(1,M);
+pValuesBoot0 = NaN(1,M);
+pValuesBoot05 = NaN(1,M);
+counter0 = 0;
+counter05 = 0;
+counterBoot0 = 0;
+counterBoot05 = 0;
 
-allValues(1:100,1:10) = X;
-allValues(1:100,11:22) = Y;
 
-iterations = 1000;
-pValues = zeros(1,100);
-meanDifferenceResults = NaN(M,iterations);
+%for different as, change the a index
 
 for i=1:M
-    for j=1:iterations
-       meanDifference1 = mean(X(i,:)) - mean(Y(i,:));
-       shuffledSample = allValues(i,:);
-       shuffledSample=shuffledSample(randperm(length(shuffledSample)));
-       newX = shuffledSample(1:n);
-       newY = shuffledSample((n+1):m);
-       meanDifference2 = mean(newX) - mean(newY);
-       if abs(meanDifference2) >= meanDifference1
-           meanDifferenceResults(i,j) = 1;
-       else
-           meanDifferenceResults(i,j) = 0;
-       end
+    [~,pValues0(1,i),~,~] = ttest(X(i,:),0, 'Alpha', a(1));
+    [~,pValues05(1,i),~,~] = ttest(X(i,:),0.5, 'Alpha', a(1));
+    bootstat = bootstrp(B,@mean,X(i,:));
+    [~,pValuesBoot0(1,i),~,~] = ttest(bootstat,0, 'Alpha', a(1));
+    [~,pValuesBoot05(1,i),~,~] = ttest(bootstat,0.5, 'Alpha', a(1));        
+
+    if pValues0(1,i) <= a(1)
+       counter0 = counter0 + 1; 
+    end
+    if pValues05(1,i) <= a(1)
+       counter05 = counter05 + 1; 
+    end
+    if pValuesBoot0(1,i) <= a(1)
+       counterBoot0 = counterBoot0 + 1; 
+    end
+    if pValuesBoot05(1,i) <= a(1)
+       counterBoot05 = counterBoot05 + 1; 
     end
 end
 
-pValue = sum(meanDifferenceResults(:) == 1) / (M * iterations)
+percentage0 = counter0 / M
+percentage05 = counter05 / M
+percentageBoot0 = counterBoot0 / M
+percentageBoot05 = counterBoot05 / M
+
