acb3885609118cdc74b31e41ab4446dc88a47089
Konstantinos Michopoulos <konstantinos.michopoulos@gmail.com>
2021-11-26 12:06:19 +0000

    Change rand to poissrnd

--- Data_Anal_Chapter3_12.m
+++ Data_Anal_Chapter3_12.m
@@ -4,13 +4,14 @@ clear
 n = 10;
 M = 100;
 B = 1000;
+lambda = 0.1;
 mu = 0;
 sigma = 1;
 a = [0.05, 0.025, 0.1];
 
 X = normrnd(mu,sigma,M,n);
 
-Y = rand(M,n);
+Y = poissrnd(lambda,M,n);
 
 pValuesParametric = NaN(1,100);
 pValuesBoot = NaN(1,100);
