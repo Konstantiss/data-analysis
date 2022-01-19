286997760fbc39945a1e8383b3629160a8293879
Konstantinos Michopoulos <konstantinos.michopoulos@gmail.com>
2022-01-19 19:58:56 +0000

    Add exercise 3 function.

---
+++ Project/Group19Exe3Fun1.m
@@ -0,0 +1,18 @@
+function difference = Group19Exe3Fun1(dailyPositivityRatesGreece, weeklyPositivityRateEurope)
+    ci = bootci(1000,@mean,dailyPositivityRatesGreece)
+    
+    if weeklyPositivityRateEurope > ci(1)
+       if weeklyPositivityRateEurope < ci(2)
+          difference = 0; 
+       else
+          difference = weeklyPositivityRateEurope - ci(2);
+       end
+    elseif weeklyPositivityRateEurope < ci(2)
+        if weeklyPositivityRateEurope > ci(1)
+            difference = 0;
+        else
+          difference = weeklyPositivityRateEurope - ci(1);
+       end
+    end
+    
+end
