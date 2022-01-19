18540b3c659026b097570e07661f9ce3934cb1c9
Konstantinos Michopoulos <konstantinos.michopoulos@gmail.com>
2022-01-19 14:20:07 +0000

    Correct exercise 1 solution.

--- Project/Group19Exe1.m
+++ Project/Group19Exe1.m
@@ -1,67 +1,96 @@
+% Konstantinos Michopoulos, webmail: michopoul@ece.auth.gr, AEM: 9839
+% Georgios Skiadas, webmail: skiadasg@ece.auth.gr, AEM: 9907
+
 clc
 clear
 
+estoniaId = 8;
 data = readtable('ECDC-7Days-Testing.xlsx');
 countries = readtable('EuropeanCountries.xlsx');
 weeks2021 = ["2021-W45" "2021-W46" "2021-W47" "2021-W48" "2021-W49" "2021-W50"];
 weeks2020 = ["2020-W45" "2020-W46" "2020-W47" "2020-W48" "2020-W49" "2020-W50"];
+estoniaData = data(contains(data.country, countries{estoniaId,2}), :);
 
 positivityRates2021 = zeros(height(countries),1);
 positivityRates2020 = zeros(height(countries),1);
 
+bestWeek2021Id = 0;
+bestWeek2020Id = 0;
+
+for j = 1:length(weeks2021)
+        weekData2021 = estoniaData(contains(estoniaData.year_week, weeks2021(j)), :);
+        maxWeekPositivityRate = max(weekData2021.positivity_rate);
+        
+        if maxWeekPositivityRate > positivityRates2021(estoniaId,1) 
+            if maxWeekPositivityRate < 100
+                positivityRates2021(estoniaId,1) = maxWeekPositivityRate;
+                bestWeek2021Id = j;
+            end
+        end
+end
+
+for j = 1:length(weeks2020)
+    weekData2020 = estoniaData(contains(estoniaData.year_week, weeks2020(j)), :);
+    maxWeekPositivityRate = max(weekData2020.positivity_rate);
+
+    if maxWeekPositivityRate > positivityRates2020(estoniaId,1)
+        if maxWeekPositivityRate < 100
+            positivityRates2020(estoniaId,1) = maxWeekPositivityRate;
+            bestWeek2020Id = j;
+        end
+    end
+end
+
+
 
 for i = 1:height(countries)
     countryData = data(contains(data.country, countries{i,2}), :);
-    for j = 1:length(weeks2021)
-        weekData2021 = countryData(contains(countryData.year_week, weeks2021(j)), :);
-        maxWeekPositivityRate = max(weekData2021.positivity_rate);
-        
-        if maxWeekPositivityRate > positivityRates2021(i,1) 
-            if maxWeekPositivityRate < 100
-                positivityRates2021(i,1) = maxWeekPositivityRate;
-            end
+    weekData2021 = countryData(contains(countryData.year_week, weeks2021(bestWeek2021Id)), :);
+    maxWeekPositivityRate2021 = max(weekData2021.positivity_rate);
+
+    if maxWeekPositivityRate2021 > positivityRates2021(i,1) 
+        if maxWeekPositivityRate2021 < 100
+            positivityRates2021(i,1) = maxWeekPositivityRate2021;
         end
     end
     
-    for j = 1:length(weeks2020)
-        weekData2020 = countryData(contains(countryData.year_week, weeks2020(j)), :);
-        maxWeekPositivityRate = max(weekData2020.positivity_rate);
+    weekData2020 = countryData(contains(countryData.year_week, weeks2020(j)), :);
+    maxWeekPositivityRate2020 = max(weekData2020.positivity_rate);
 
-        if maxWeekPositivityRate > positivityRates2020(i,1)
-            if maxWeekPositivityRate < 100
-                positivityRates2020(i,1) = maxWeekPositivityRate;
-            end
+    if maxWeekPositivityRate2020 > positivityRates2020(i,1)
+        if maxWeekPositivityRate2020 < 100
+            positivityRates2020(i,1) = maxWeekPositivityRate2020;
         end
     end
 end
 
 figure()
-title('Maximum positivity rates for week 45-50, 2021')
 histogram(positivityRates2021, 20)
+title('Maximum positivity rates for week 45-50, 2021')
+
 
 figure()
-title('Maximum positivity rates for week 45-50, 2021 - exponential distribution fit')
 histfit(positivityRates2021, 20, 'exponential')
+title('Maximum positivity rates for week 45-50, 2021 - exponential distribution fit')
 
 figure()
-title('Maximum positivity rates for week 45-50, 2021 - normal distribution fit')
 histfit(positivityRates2021, 20, 'normal')
+title('Maximum positivity rates for week 45-50, 2021 - normal distribution fit')
 
 figure()
-title('Maximum positivity rates for week 45-50, 2020')
 histogram(positivityRates2020, 20)
+title('Maximum positivity rates for week 45-50, 2020')
 
 figure()
-title('Maximum positivity rates for week 45-50, 2020 - exponential distribution fit')
 histfit(positivityRates2020, 20, 'exponential')
+title('Maximum positivity rates for week 45-50, 2020 - exponential distribution fit')
 
 figure()
-title('Maximum positivity rates for week 45-50, 2020 - normal distribution fit')
 histfit(positivityRates2020, 20, 'normal')
+title('Maximum positivity rates for week 45-50, 2020 - normal distribution fit')
 
 fitdist(positivityRates2021, 'Normal')
 fitdist(positivityRates2021, 'Exponential')
 
 fitdist(positivityRates2020, 'Normal')
 fitdist(positivityRates2020, 'Exponential')
-
