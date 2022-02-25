4607b2516d63c5a8c316e170eb7829fa350f8d57
Konstantiss <konstantinos.michopoulos@gmail.com>
2022-02-25 13:07:47 +0200

    Change solution of exercise 4.

--- Project/Group19Exe4Prog1.m
+++ Project/Group19Exe4Prog1.m
@@ -22,84 +22,60 @@ positivityRates2020 = estoniaData2020.positivity_rate;
 positivityRates2021 = reshape(positivityRates2021,[],length(positivityRates2021));
 positivityRates2020 = reshape(positivityRates2020,[],length(positivityRates2020));
 
-
-mean(positivityRates2021)
-mean(positivityRates2020)
-
-[h,p,ci,stats] = ttest2(positivityRates2021,positivityRates2020)
+meanDiffOriginal = mean(positivityRates2021) -  mean(positivityRates2020);
+h_parametric = ones(5,1);
+p_parametric = ones(5,1);
+[h_parametric(1),p_parametric(1),ci,stats] = ttest2(positivityRates2021,positivityRates2020);
 
 %Resample data.
-[~,bootsam] = bootstrp(M,[],positivityRates2021);
-bootsam = reshape(bootsam, length(bootsam), []);
-positivityRates2021Resampled = positivityRates2021(bootsam);
+positivityRates2021boot = bootstrp(M,@mean,positivityRates2021);
+positivityRates2020boot = bootstrp(M,@mean,positivityRates2020);
 
-[~,bootsam] = bootstrp(M,[],positivityRates2020);
-bootsam = reshape(bootsam, length(bootsam), []);
-positivityRates2020Resampled = positivityRates2020(bootsam);
-pvaluesEstoniaBoot = zeros(1,M);
+meanDiffBoot = positivityRates2021boot - positivityRates2020boot;
 
-correlatedSamplesEstonia = 0;
-for i=1:M
-    [h,pvaluesEstoniaBoot(1,i),ci,stats] = ttest2(positivityRates2021Resampled(i,:),positivityRates2020Resampled(i,:));
-    %Check if ci contains 0.
-    if ci(1) < 0 && ci(2) > 0
-        correlatedSamplesEstonia = correlatedSamplesEstonia + 1;
-    end
+alpha = 0.05;
+limits = floor([M*alpha/2 M*(1-alpha/2)]);
+
+h = ones(5,1);
+
+if meanDiffBoot(limits(1)) <= meanDiffOriginal && meanDiffOriginal <= meanDiffBoot(limits(2))
+    h(1) = 0;
 end
-correlationPercentage = (correlatedSamplesEstonia / M) * 100;
-fprintf("Correlated samples percentage for Estonia: %0.2f ",correlationPercentage);
 
 %% Other countries
-countryIds = [estoniaId - 4 estoniaId - 3 estoniaId - 2 estoniaId - 1];
+countryIds = [estoniaId - 2 estoniaId - 1 estoniaId + 1 estoniaId + 3];
 
-positivityRatesCountries2021 = zeros(length(countryIds),length(weeks2021));
-positivityRatesCountries2020 = zeros(length(countryIds),length(weeks2020));
+% positivityRatesCountries2021 = zeros(length(countryIds),length(weeks2021));
+% positivityRatesCountries2020 = zeros(length(countryIds),length(weeks2020));
 countryPValues = zeros(length(countryIds),1);
 bootCountryPValues = zeros(length(countryIds), M);
 correlatedSamplesCountries = zeros(length(countryIds),1);
 
 for i=1:length(countryIds)
     countryData = data(contains(data.country, countries{countryIds(i),2}), :);
+    
     for j=1:length(weeks2021)
         weekData2021 = countryData(contains(countryData.year_week, weeks2021(j)), :);
-        positivityRatesCountries2021(i,j) = mean(weekData2021.positivity_rate);
+        positivityRatesCountries2021(j) = mean(weekData2021.positivity_rate);
         
         weekData2020 = countryData(contains(countryData.year_week, weeks2020(j)), :);
-        positivityRatesCountries2020(i,j) = mean(weekData2020.positivity_rate);
+        positivityRatesCountries2020(j) = mean(weekData2020.positivity_rate);
     end
     
-    [h,countryPValues(i,1),ci,stats] = ttest2(positivityRatesCountries2021(i,:),positivityRatesCountries2020(i,:));
+    [h_parametric(i+1),p_parametric(i+1),~,~] = ttest2(positivityRatesCountries2021,positivityRatesCountries2020);
     
-    %Resample data.
-    temp = positivityRatesCountries2021(i,:);
-    [~,bootsam] = bootstrp(M,[],temp);
-    bootsam = reshape(bootsam, length(bootsam), []);
-    positivityRatesCountry2021Resampled = temp(bootsam);
+    meanDiffOriginal = mean(positivityRatesCountries2021) - mean(positivityRatesCountries2021);
     
-    temp = positivityRatesCountries2020(i,:);
-    [~,bootsam] = bootstrp(M,[],temp);
-    bootsam = reshape(bootsam, length(bootsam), []);
-    positivityRatesCountry2020Resampled = temp(bootsam);
-    for j=1:M
-        [h,bootCountryPValues(i,j),ci,stats] = ttest2(positivityRatesCountry2021Resampled(j,:),positivityRatesCountry2020Resampled(j,:));
-        %Check if ci contains 0.
-        if ci(1) < 0 && ci(2) > 0
-            correlatedSamplesCountries(i,1) = correlatedSamplesCountries(i,1) + 1;
-        end
+    positivityRatesCountries2021Boot = bootstrp(M,@mean,positivityRatesCountries2021);
+    positivityRatesCountries2020Boot = bootstrp(M,@mean,positivityRatesCountries2020);
+    
+    meanDiffBoot = positivityRatesCountries2021Boot - positivityRatesCountries2020Boot;
+    
+    if meanDiffBoot(limits(1)) <= meanDiffOriginal && meanDiffOriginal <= meanDiffBoot(limits(2))
+        h(i+1) = 0;
     end
+   
 end
-
-figure()
-histogram(pvaluesEstoniaBoot,20)
-title('P values for resampled data of Estonia')
-
-figure()
-histogram(countryPValues,4)
-title('P values for 4 europe countries')
-
-figure()
-histogram(bootCountryPValues,20)
-title('P values for resampled data of 4 europe countries')
 
 
 %We come to the conclusion that the positivity rates of 2021 and 2020
